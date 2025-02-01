95bd642682ac129d2cc9015444cf6faa5e68a37f
DESKTOP-C6FKCTH\mistr <Tejron>
2025-02-01 14:18:28 +0100

    Player simple movement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] int movementSpeed;
+
+    private Vector2 movementInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       movementInput.x = Input.GetAxisRaw("Horizontal");
+       movementInput.y = Input.GetAxisRaw("Vertical");
+
+       transform.position += new Vector3(movementInput.x, movementInput.y, 0f) * movementSpeed * Time.deltaTime;
+
+    }
+}
