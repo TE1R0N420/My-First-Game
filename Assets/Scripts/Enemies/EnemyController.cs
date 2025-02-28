b4baaaf6750ef69f9b28a602f86103077e5929db
DESKTOP-C6FKCTH\mistr <Tejron>
2025-02-28 17:13:13 +0100

    Enemy moving + animations

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -9,14 +9,23 @@ public class EnemyController : MonoBehaviour
     private Rigidbody2D enemyRigidbody;
 
     [SerializeField] float playerChaseRange;
+    [SerializeField] float playerKeepChaseRange;
+
+    bool isChasing;
+
     private Vector3 directionToMoveIn;
 
 
     private Transform playerToChase;
 
+    private Animator enemyAnimator;
+
     void Start()
     {
+        enemyRigidbody = GetComponent<Rigidbody2D>();
         playerToChase = FindAnyObjectByType<PlayerController>().transform;
+
+        enemyAnimator = GetComponentInChildren<Animator>();
     }
 
 
@@ -24,9 +33,41 @@ public class EnemyController : MonoBehaviour
     {
         if (Vector3.Distance(transform.position, playerToChase.position) < playerChaseRange)
         {
-            
+            directionToMoveIn = playerToChase.position - transform.position;
+            isChasing = true;
         }
-        
+        else if (Vector3.Distance(transform.position, playerToChase.position) < playerKeepChaseRange && isChasing) 
+        {
+            directionToMoveIn = playerToChase.position - transform.position;
+        } 
+        else
+        {
+            directionToMoveIn = Vector3.zero;
+            isChasing = false;
+        }
+
+        directionToMoveIn.Normalize();
+        enemyRigidbody.linearVelocity = directionToMoveIn * enemySpeed;
+
+        if (directionToMoveIn != Vector3.zero)
+        {
+            enemyAnimator.SetBool("isWalking", true);
+        }
+        else
+        {
+            enemyAnimator.SetBool("isWalking", false);
+        }
+
+        if (playerToChase.position.x < transform.position.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+        }
+
+
     }
         
 
@@ -34,6 +75,9 @@ public class EnemyController : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, playerChaseRange);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, playerKeepChaseRange);
     }
 
 
