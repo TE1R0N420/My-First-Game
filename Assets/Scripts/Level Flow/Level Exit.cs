22d5612dfcdcbf4990cc55dc1dc65f57b54d7d04
DESKTOP-C6FKCTH\mistr <Tejron>
2025-03-14 17:10:21 +0100

    Level Change

---
+++ Assets/Scripts/Level Flow/Level Exit.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] string LevelToLoad;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            SceneManager.LoadScene(LevelToLoad);
+        }
+    }
+}
