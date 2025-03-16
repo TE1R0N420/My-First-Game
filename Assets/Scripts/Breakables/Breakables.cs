4a38622dfb05907f8074a5d5095920c88bffe36f
DESKTOP-C6FKCTH\mistr <Tejron>
2025-03-16 12:35:55 +0100

    Instantiate Broken Pieces

--- Assets/Scripts/Breakables/Breakables.cs
+++ Assets/Scripts/Breakables/Breakables.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 public class Breakables : MonoBehaviour
 {
-
+    [SerializeField] GameObject[] brokenParts;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -14,13 +14,25 @@ public class Breakables : MonoBehaviour
             bool playerIsDashing = collision.gameObject.GetComponent < PlayerController>().PlayerIsDashing();
 
             if(playerIsDashing)
-            GetComponent<Animator>().SetTrigger("Break");
+            {
+                GetComponent<Animator>().SetTrigger("Break");
+
+                for(int i = 0; i < brokenParts.Length; i++)
+                {
+                    int randomBrokenPart = Random.Range(0, brokenParts.Length);
+                    Instantiate(brokenParts[randomBrokenPart], transform.position, transform.rotation);
+                }
+
+                
+            }
+            
         }
     }
 
     public void Destroy()
     {
-      Destroy(gameObject);
+    
+        Destroy(gameObject);
     }
 
 }
